8f9c4b78dfdd17c6a03bd08655cf10eaf8514370
PetrusHimself <tlhomedipetrus@gmai.com>
2022-09-22 11:28:30 -0700

    Corrections made on print number 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,33 @@
 #include "main.h"
-/**
- * print_number - prints an integer
- * @n:.input integer parameter
- *
- *
- */
-void print_number(int n)
-{
-	unsigned int i = n;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		i = -i;
-	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+{	print_number(98);
+
+		_putchar('\n');
+
+			print_number(402);
+
+				_putchar('\n');
+
+					print_number(1024);
+
+						_putchar('\n');
+
+							print_number(0);
+
+								_putchar('\n');
+									print_number(-98);										_putchar('\n');
+return 0;
 }
