6555bd23e64d12e2a6fcbdf280f5056ec9e1eb5b
Petrus Tlhomedi <tlhomedipetrus@gmail.com>
2022-10-28 04:27:28 +0200

    Corrections

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -27,21 +27,18 @@ return (i);
 */
 
 unsigned int binary_to_uint(const char *b)
-{
-	unsigned int n = 0;
-	int i, len;
-
-	if (b == NULL)
-		return (0);
+{  unsigned int n = 0;
+    int i, len;
+      if (b == NULL)
+       return (0);
 
 	len = _strlen(b);
 
 	for (i = 0; i != len; i++)
-	{
-		if (b[len - i - 1] == '1')
-			n += 1 << i;
+	{  if (b[len - i - 1] == '1')
+		n += 1 << i;
 		else if (b[len - i - 1] != '0')
-			return (0);
+		  return (0);
 	}
 
 	return (n);
