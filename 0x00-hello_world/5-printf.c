f418668a1462486cc19889e39e406766225f68ad
PetrusHimself <tlhomedipetrus@gmail.com>
2023-05-18 09:40:49 -0700

    a C program that prints

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - msin function of my project
+ *
+ * Retun: return 0 everytime
+ *
+ */
+
+int main () {
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
