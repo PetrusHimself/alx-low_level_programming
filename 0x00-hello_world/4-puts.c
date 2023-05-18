d073a47cbd47ad5cdadb679b0e11c5778e8836ef
PetrusHimself <tlhomedipetrus@gmail.com>
2023-05-18 09:30:38 -0700

    rewrote the code to meet checker requirements

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-/**
-*main - Main function of my project
-*
-*
-*Return: return 0 everytime
-*/
 
-int main(void) {
-    const char *message = "\"Programming is like building a multilingual puzzle";
-    puts(message);
-    return (0);
+/**
+ *  main - prints a given phrase
+ *  Return: 0 if exited properly, non-zero otherwise
+ */
+
+int  main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
