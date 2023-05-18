e031cc891424893a7288703c3d01636872b65992
PetrusHimself <tlhomedipetrus@gmail.com>
2023-05-18 10:05:36 -0700

    a C program that prints the size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * Main - this is my main function
+ *
+ * Return - always return 0
+ *
+ *
+ */
+
+int main() {
+    printf("The size of a char: %d bytes\n", sizeof(char));
+    printf("The size of a int: %d bytes\n", sizeof(int));
+    printf("The size of a long int: %d bytes\n", sizeof(long int));
+    printf("The size of a long long int: %d bytes\n", sizeof(long long int));
+    printf("The size of a float: %d bytes\n", sizeof(float));
+    return 0;
+}
