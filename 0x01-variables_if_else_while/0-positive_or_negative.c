7483accb5c8aa35071bdcf68ae0e38449df00637
PetrusHimself <tlhomedipetrus@gmail.com>
2023-05-19 09:28:39 -0700

    program will assign a random number to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * main - main block
+ * main - main function
  * Description: Get a random number and print the number
  * and if it is positive, negative, or zero
  * Return: 0
