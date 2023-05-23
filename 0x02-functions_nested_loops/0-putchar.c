e6f9621838a30ee5515295761afb09cb51c86b48
PetrusHimself <tlhomedipetrus@gmail.com>
2023-05-23 07:37:12 -0700

    add the putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - this is the main block
+ *
+ * putchar - prints a character
+ *
+ */
+
+int main(void) {
+    int ch = '_putchar';
+    putchar(ch);
+    
+    return (0);
+}
