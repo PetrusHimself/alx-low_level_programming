ae493c581476e3b1730c7c99b241edbb93625ee6
Petrus Tlhomedi <tlhomedipetrus@gmail.com>
2023-05-31 01:23:39 +0200

    Prints

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
 /**
- *  * _puts - print a string to stdout
- *   * @str: char array string type
- *    * Description: Can only use _putchar
- *     */
+
+ * _puts - print a string to stdout
+
+ * @str: char array string type
+
+ * Description: Can only use _putchar
+
+ */
 
 void _puts(char *str)
-{
-		int i;
 
-			for (i = 0; str[i] != '\0'; i++)
-					{
-								_putchar(str[i]);
-									}
-				_putchar('\n');
+{
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+
+	{
+
+		_putchar(str[i]);
+
+	}
+
+	_putchar('\n');
+
 }
